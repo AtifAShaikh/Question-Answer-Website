52a5fd64f1f11d49ca404226cee110b11394e16f
chris interiano <cinteriano@live.com>
2021-04-16 11:48:27 -0700

    made changes to routes

--- routes/api/answers.js
+++ routes/api/answers.js
@@ -1,46 +1,59 @@
 const router = require("express").Router();
-const booksController = require("../../controllers/booksController");
-const answers = require('../../models/answers');
+const answersController = require("../../controllers/answersController");
+// const answers = require('../../models/answers');
 
 
-router.post('/create', (req, res) => {
-    
-    answers.create(req.body)
-        .then(result =>
-            res.status(200).json(result))
-        .catch(err =>
-            res.status(400).json(err))
-    
-    
-    
-    console.log('api/answers/create route hit');
-   
-});
-
-router.post('/favorite', (req, res) => {
-    console.log('api/answers/favorite route hit');
-    res.json({message: "successful favorite"});
-});
-
-router.post('/upvote', (req, res) => {
-    console.log('api/answers/upvote route hit');
-    res.json({message: "success"});
-});
-
-router.post('/downvote', (req, res) => {
-    console.log('api/answers/downvote route hit');
-    res.json({message: "successful downvote"});
-});
-
-router.post('/follow', (req, res) => {
-    console.log('api/answers/follow route hit');
-    res.json({message: "successful follow"});
-});
-
-router.get('/find', (req, res) => {
-    console.log('api/answers/find route hit');
-    res.json({message: "successful find"});
-});
-
-
+// Matches with "/api/posts"
+router
+  .route("/")
+  .get(answersController.findAll)
+  .post(answersController.create);
+// Matches with "/api/posts/:id"
+router
+  .route("/:id")
+  .get(answersController.findById)
+  .put(answersController.update)
+  .delete(answersController.remove);
 module.exports = router;
+
+// router.post('/create', (req, res) => {
+    
+//     answers.create(req.body)
+//         .then(result =>
+//             res.status(200).json(result))
+//         .catch(err =>
+//             res.status(400).json(err))
+    
+    
+    
+//     console.log('api/answers/create route hit');
+   
+// });
+
+// router.post('/favorite', (req, res) => {
+//     console.log('api/answers/favorite route hit');
+//     res.json({message: "successful favorite"});
+// });
+
+// router.post('/upvote', (req, res) => {
+//     console.log('api/answers/upvote route hit');
+//     res.json({message: "success"});
+// });
+
+// router.post('/downvote', (req, res) => {
+//     console.log('api/answers/downvote route hit');
+//     res.json({message: "successful downvote"});
+// });
+
+// router.post('/follow', (req, res) => {
+//     console.log('api/answers/follow route hit');
+//     res.json({message: "successful follow"});
+// });
+
+// router.get('/find', (req, res) => {
+//     console.log('api/answers/find route hit');
+//     res.json({message: "successful find"});
+// });
+
+
+// module.exports = router;
--- routes/api/questions.js
+++ routes/api/questions.js
@@ -1,38 +1,52 @@
 const router = require("express").Router();
-const booksController = require("../../controllers/booksController");
-const Questions = require('../../models/questions');
+const questionsController = require("../../controllers/questionsController");
+// const Questions = require('../../models/questions');
 
-router.post('/create', (req, res) => {
-    Questions.create(req.body)
-        .then(result =>
-            res.status(200).json(result))
-        .catch(err =>
-            res.status(400).json(err))
-
-    console.log('api/questions/create route hit');
-    
-});
-
-
-router.post('/upvote', (req, res) => {
-    console.log('api/questions/upvote route hit');
-    res.json({ message: "successfull upvote" });
-});
-
-router.post('/downvote', (req, res) => {
-    console.log('api/questions/downvote route hit');
-    res.json({ message: "successful downvote" });
-});
-
-router.post('/follow', (req, res) => {
-    console.log('api/questions/follow route hit');
-    res.json({ message: "successesful follow" });
-});
-
-router.get('/find', (req, res) => {
-    console.log('api/questions/find route hit');
-    res.json({ message: "successeful find" });
-});
-
-
+// Matches with "/api/posts"
+router
+  .route("/")
+  .get(questionsController.findAll)
+  .post(questionsController.create);
+// Matches with "/api/posts/:id"
+router
+  .route("/:id")
+  .get(questionsController.findById)
+  .put(questionsController.update)
+  .delete(questionsController.remove);
 module.exports = router;
+
+
+// router.post('/create', (req, res) => {
+//     Questions.create(req.body)
+//         .then(result =>
+//             res.status(200).json(result))
+//         .catch(err =>
+//             res.status(400).json(err))
+
+//     console.log('api/questions/create route hit');
+    
+// });
+
+
+// router.post('/upvote', (req, res) => {
+//     console.log('api/questions/upvote route hit');
+//     res.json({ message: "successfull upvote" });
+// });
+
+// router.post('/downvote', (req, res) => {
+//     console.log('api/questions/downvote route hit');
+//     res.json({ message: "successful downvote" });
+// });
+
+// router.post('/follow', (req, res) => {
+//     console.log('api/questions/follow route hit');
+//     res.json({ message: "successesful follow" });
+// });
+
+// router.get('/find', (req, res) => {
+//     console.log('api/questions/find route hit');
+//     res.json({ message: "successeful find" });
+// });
+
+
+// module.exports = router;
