e59bce23c3e1d1c3338309742a0637c789ec62fe
E Alexander <erick.interiano05@gmail.com>
2021-04-16 14:01:31 -0700

    deleted a few lines that werent needed

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const usersController = require("../../controllers/usersController");
-// const User  = require('../../models/user');
+
 
 // Matches with "/api/posts"
 router
